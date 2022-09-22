a70a8330f7f9c8b7690d154c9de2886296848557
gabrielvalizi_ <gabrielvalizi@usp.br>
2022-09-22 12:49:48 -0300

    Codigo de Fibonaccii com recursao

--- fibonaccii.c
+++ fibonaccii.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 
+int fibonacci(int n){
+
+  int variavel;
+  if(n <= 0){
+    variavel = 0;
+  }
+  if(n == 1){
+    variavel = 1;
+  }
+  if(n>1){
+    variavel = fibonacci(n-1)+fibonacci(n-2);
+   }
+
+  return variavel;
+  
+  }
+
 int main(void){
 
-  int variavel_1 = 0, variavel_2 = 1;
-  int a,b=1;
-  
+  int a,b;
+
   printf("Digite um valor\n");
   scanf("%d",&a);
-  
-  for(int i=0;i<=a-1;i++){
-    
-    if(a-1<0){
 
-      b=0;
-      break;
+  b = fibonacci(a);
 
-    }
-
-    variavel_1=variavel_2;
-    variavel_2=b;
-    b=variavel_1+variavel_2;
-
-  }
-  
-  printf("O %d-esimo valor da sequencia vale %d",a,variavel_1);
+  printf("O %d-esimo valor da sequencia vale %d",a,b);
   printf("\n");
+
   return 0;
 
 }
